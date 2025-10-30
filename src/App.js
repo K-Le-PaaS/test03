86885bb16aedb9d9f1b56ced2733f141e64db3b5
Cho YunHo <code.penguin.yoon@gmail.com>
2025-10-30 17:26:37 +0900

    첫번째 커밋입니다.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>K-Le PaaS Frontend Test</h1>
-        <p>쿠버네티스 자동화 배포 테스트용 프런트엔드 애플리케이션</p>
+        <p>K-PaaS 쿠버네티스 자동화 배포 테스트용 프런트엔드 애플리케이션</p>
         <div className="status">
           <span className="status-indicator">🟢</span>
           <span>첫번째 이미지입니다.</span>
