fa0a5de99426b3b6597d6c178182d3bdc7abd418
Cho YunHo <code.penguin.yoon@gmail.com>
2025-10-31 01:00:33 +0900

    두번째 커밋입니다.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>K-Le PaaS Frontend Test</h1>
+        <h1>K-Le-PaaS Frontend Test</h1>
         <p>K-PaaS 쿠버네티스 자동화 배포 테스트용 프런트엔드 애플리케이션</p>
         <div className="status">
           <span className="status-indicator">🟢</span>
-          <span>첫번째 이미지입니다.</span>
+          <span>두번째 이미지입니다.</span>
         </div>
       </header>
     </div>
